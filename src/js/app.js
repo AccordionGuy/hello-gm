3a0b59e1563441f092a2cabfd06e65cbe8fbbab1
Joey deVilla <joey@joeydevilla.com>
2017-02-26 23:12:12 -0500

    First commit

---
+++ src/js/app.js
@@ -0,0 +1,28 @@
+// Your code goes here
+
+var useMetricUnits = true
+changeUnits(document.getElementById('unitSwitch'))
+
+var vinElem = document.getElementById('vin');
+gm.info.getVehicleConfiguration(function(data) {
+  vinElem.innerHTML = gm.info.getVIN();
+});
+
+
+function showSpeed(data) {
+  console.log("showSpeed - metric? " + useMetricUnits)
+  var speed = data.average_speed
+  if ( speed !== undefined ) {
+    var speedText = document.getElementById('speed')
+    speed = useMetricUnits ? speed : Math.round(speed * 0.621)
+    speedText.innerHTML = speed
+  }
+}
+
+function changeUnits(checkbox) {
+  console.log("Changed!" + checkbox.checked)
+  useMetricUnits = checkbox.checked
+  gm.info.getVehicleData(showSpeed, ['average_speed'])
+}
+
+gm.info.watchVehicleData(showSpeed, ['average_speed'])
